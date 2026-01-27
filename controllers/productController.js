6b83922446ee22b31be7345f50b0c4f6e5d745c8
Gabriel <gakirakiyohara@gmail.com>
2026-01-27 17:54:32 -0300

    upando

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,30 @@
 const Product = require("../models/Product");
 
 exports.getAll = async (req, res) => {
-  const products = await Product.find().populate("category");
-  res.json(products);
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.create = async (req, res) => {
-  const product = new Product(req.body);
-  await product.save();
-  res.status(201).json(product);
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 exports.getByCategory = async (req, res) => {
-  const products = await Product.find({ category: req.params.categoryId });
-  res.json(products);
+  try {
+    const products = await Product.findAll({
+      where: { category: req.params.categoryId },
+    });
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
