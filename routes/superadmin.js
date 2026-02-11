507264f1f5ecd3a580cc474317401a9604c5158d
Gabriel <gakirakiyohara@gmail.com>
2026-02-11 18:51:06 -0300

    import fix superadmin

--- routes/superadmin.js
+++ routes/superadmin.js
@@ -1,7 +1,7 @@
-const express = require("express");
+import express from "express";
+import knex from "knex";
+import path from "path";
 const router = express.Router();
-const knex = require("knex");
-const path = require("path");
 
 console.log("[DEBUG] superadmin.js carregado!");
 
